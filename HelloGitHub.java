6b651e6c2c19e00424c24d37abfe9984b93b07b5
Natalie Jenh M. Alarcon <127457468+nachaley28@users.noreply.github.com>
2025-02-10 21:43:20 +0800

    Update HelloGitHub.java

--- HelloGitHub.java
+++ HelloGitHub.java
@@ -1,10 +1,14 @@
 import java.time.LocalDateTime;  
 import java.time.format.DateTimeFormatter;  
+import java.util.Scanner;
 public class HelloGitHub{
   public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter your name: ");
+    String name = sc.nextLine();
   
 
-System.out.print("Hello GitHUB");
+System.out.print("Hello GitHUB Hello," + name);
 LocalDateTime now = LocalDateTime.now();
 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 System.out.println("Current Date and Time: " + now.format(formatter));
